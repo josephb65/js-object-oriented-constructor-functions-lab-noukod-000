ce3a1080a3dcd064333a3fc2aa5f976ebc41fcc0
Billy Joseph <ingbillyjoseph@gmail.com>
2019-11-08 15:31:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+const driver = {}
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+  }
+  
+  function deleteFromDriverByKey(driver, key){
+    const newdriver = { driver };
+    delete newdriver[key];
+    return newdriver;
+  }
+  
+  function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+  }
